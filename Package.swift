df3760ab37a25a4c8143979bcee1ce491f9bd458
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2022-12-18 20:25:32 +0000

    swift-tools-version: 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -26,5 +26,6 @@ let package = Package(
         .testTarget(
             name: "SpeedManagerModuleTests",
             dependencies: ["SpeedManagerModule"]),
-    ]
+    ],
+    swiftLanguageVersions: [SwiftVersion.v5]
 )
