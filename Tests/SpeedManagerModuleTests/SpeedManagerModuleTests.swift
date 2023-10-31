0b2e2dc7e8c97b82af8024e27ed79e26468a0453
Bill <yiu.chan@florio.com>
2023-10-31 11:54:39 +0100

    test_speedAccuracy

--- Tests/SpeedManagerModuleTests/SpeedManagerModuleTests.swift
+++ Tests/SpeedManagerModuleTests/SpeedManagerModuleTests.swift
@@ -2,21 +2,34 @@ import XCTest
 @testable import SpeedManagerModule
 
 final class SpeedManagerModuleTests: XCTestCase {
-    
+
     var manager: SpeedManager?
-    
+
     func test_speed() throws {
         let mockDelegate = SpeedManagerDelegateMock(testCase: self)
         manager = SpeedManager(.kilometersPerHour, trigger: self)
         manager?.delegate = mockDelegate
-        
+
         mockDelegate.expectSpeed()
         manager?.startUpdatingSpeed()
-        
+
         waitForExpectations(timeout: 1)
-        
+
         let result = try XCTUnwrap(mockDelegate.speed)
         XCTAssertEqual(result, 12.2)
+    }
+
+    func test_speedAccuracy() throws {
+        let mockDelegate = SpeedManagerDelegateMock(testCase: self)
+        manager = SpeedManager(.kilometersPerHour, trigger: self)
+        manager?.delegate = mockDelegate
+
+        mockDelegate.expectSpeed()
+        manager?.startUpdatingSpeed()
+
+        waitForExpectations(timeout: 1)
+
+        XCTAssertEqual(mockDelegate.speedAccuracy, 1)
     }
 }
 
@@ -24,7 +37,8 @@ extension SpeedManagerModuleTests: SpeedManagerTrigger {
     func startMonitoringSpeed() {
         guard let manager = manager else { return }
         self.manager?.delegate?.speedManager(manager,
-                                             didUpdateSpeed: 12.2, speedAccuracy: 1)
+                                             didUpdateSpeed: 12.2, 
+                                             speedAccuracy: 1)
     }
     
     func startUpdatingSpeed() {
@@ -32,9 +46,11 @@ extension SpeedManagerModuleTests: SpeedManagerTrigger {
     }
 }
 
-class SpeedManagerDelegateMock: SpeedManagerDelegate {    
+class SpeedManagerDelegateMock: SpeedManagerDelegate {
    
     var speed: Double?
+    var speedAccuracy: Double?
+
     private var expectation: XCTestExpectation?
     private let testCase: XCTestCase
     
@@ -44,7 +60,10 @@ class SpeedManagerDelegateMock: SpeedManagerDelegate {
     func speedManager(_ manager: SpeedManagerModule.SpeedManager, didUpdateSpeed speed: Double, speedAccuracy: Double) {
         didUpdateSpeed = true
         
-        if expectation != nil { self.speed = speed }
+        if expectation != nil {
+            self.speed = speed
+            self.speedAccuracy = speedAccuracy
+        }
         expectation?.fulfill()
         expectation = nil
     }
