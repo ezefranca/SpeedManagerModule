ce2835f75929cec89c6fc4ed2c8fefee9f3f83fa
Ezequiel Santos <ezequiel.santos@miniclip.com>
2024-07-23 11:07:10 +0100

    update Demo

--- Demo/Demo/Demo/ContentView.swift
+++ Demo/Demo/Demo/ContentView.swift
@@ -2,10 +2,18 @@ import SwiftUI
 import SpeedManagerModule
 import SpeedometerSwiftUI
 
+extension Double {
+    /// Returns the double value fixed to one decimal place.
+    func fixedToOneDecimal() -> String {
+        return String(format: "%.1f", self)
+    }
+}
+
 struct ContentView: View {
     @StateObject var speedManager = SpeedManager(speedUnit: .kilometersPerHour)
     @State var progress: CGFloat = 0.0
-    let maxSpeed: CGFloat = 200.0
+    @State private var speed: TimeInterval = 0.01
+    let maxSpeed: CGFloat = 200.0 // Define a constant maximum speed
 
     var body: some View {
         VStack {
@@ -16,18 +24,17 @@ struct ContentView: View {
                 Text("\(speedManager.speed.fixedToOneDecimal()) km/h")
                     .monospaced()
                 
-                GaugeView(
-                    animationDuration: 0.1,
-                    progress: progress,
-                    numberOfSegments: 200,
-                    step: 20
-                )
-                .frame(width: 300, height: 300)
-                .onAppear {
-                    updateProgress()
-                }
-                .onChange(of: speedManager.speed) { newSpeed in
-                    updateProgress()
+                TimelineView(.animation(minimumInterval: speed)) { context in
+                    GaugeView(
+                        animationDuration: speed,
+                        progress: progress,
+                        numberOfSegments: 200,
+                        step: 20
+                    )
+                    .onChange(of: context.date) { oldValue, newValue in
+                        updateProgress()
+                    }
+                    .frame(width: 300, height: 300)
                 }
                 
             default:
@@ -41,18 +48,12 @@ struct ContentView: View {
         withAnimation(.easeInOut(duration: 0.5)) {
             progress = CGFloat(speedManager.speed / maxSpeed)
         }
+        speed = 0.01 // Adjust speed interval as needed
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-extension Double {
-    /// Returns the double value fixed to one decimal place.
-    func fixedToOneDecimal() -> String {
-        return String(format: "%.1f", self)
     }
 }
