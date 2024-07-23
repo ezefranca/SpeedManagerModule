4016631850922e9dadce80e077eab0e134a8ab47
Ezequiel Santos <ezequiel.santos@miniclip.com>
2024-07-23 10:35:01 +0100

    Update Demo

--- Demo/Demo/Demo/ContentView.swift
+++ Demo/Demo/Demo/ContentView.swift
@@ -1,30 +1,45 @@
-//
-//  ContentView.swift
-//  Demo
-//
-//  Created by Ezequiel Santos on 19/12/2022.
-//
-
 import SwiftUI
 import SpeedManagerModule
+import SpeedometerSwiftUI
 
 struct ContentView: View {
     @StateObject var speedManager = SpeedManager(speedUnit: .kilometersPerHour)
-    
+    @State var progress: CGFloat = 0.0
+    let maxSpeed: CGFloat = 200.0
+
     var body: some View {
         VStack {
             switch speedManager.authorizationStatus {
             case .authorized:
                 Text("Your current speed is:")
-                Text("\(speedManager.speed.fixedToOneDecimal())")
-                Text("km/h")
+                    .monospaced()
+                Text("\(speedManager.speed.fixedToOneDecimal()) km/h")
+                    .monospaced()
                 
-                CustomGauge(currentSpeed: $speedManager.speed)
+                GaugeView(
+                    animationDuration: 0.1,
+                    progress: progress,
+                    numberOfSegments: 200,
+                    step: 20
+                )
+                .frame(width: 300, height: 300)
+                .onAppear {
+                    updateProgress()
+                }
+                .onChange(of: speedManager.speed) { newSpeed in
+                    updateProgress()
+                }
                 
             default:
                 Text("Check your location permissions...")
                 ProgressView()
             }
+        }
+    }
+
+    private func updateProgress() {
+        withAnimation(.easeInOut(duration: 0.5)) {
+            progress = CGFloat(speedManager.speed / maxSpeed)
         }
     }
 }
@@ -35,3 +50,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+extension Double {
+    /// Returns the double value fixed to one decimal place.
+    func fixedToOneDecimal() -> String {
+        return String(format: "%.1f", self)
+    }
+}
